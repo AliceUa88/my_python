e5ded120c161d442904a1d7c5f39c8cec4d3e279
Alice <124494763+AliceUa88@users.noreply.github.com>
2025-12-19 22:18:42 +0200

    додала можливість самостійно додавати товари

--- git/online_shop/app/routes.py
+++ git/online_shop/app/routes.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import db, Product
 bp=Blueprint('routes', __name__)
@@ -16,9 +17,22 @@ def product():
 @bp.route('/add', methods=['GET', 'POST'])
 def add_product():
     if request.method == 'POST':
-        name=request.form['name']
-        price=request.form['price']
-        product=Product(name=name, price=float(price))
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form.get('description')
+        stock = request.form.get('stock', type=int)
+        category = request.form.get('category')
+        rating = request.form.get('rating', type=float)
+        product = Product(
+            name=name,
+            price=float(price),
+            description=description,
+            stock=stock or 0,
+            is_active='is_active' in request.form, 
+            category=category,
+            rating=rating or 0.0,
+            sale='sale' in request.form  
+)
         db.session.add(product)
         db.session.commit()
         flash("Product added")
